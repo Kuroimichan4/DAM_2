64ea7a72d544f74f273f686d74e9917e1eb6e2ce
Kuroimichan4 <miriamhernav@campus.monlau.com>
2025-12-31 16:16:36 +0100

    parctica y actu de interfaces

---
+++ Desarrollo_de_Interficies/RA2 - Asistente de Voz/AsistenteVoz/AsistenteVoz/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace AsistenteVoz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.labelTextoReconocido = new System.Windows.Forms.Label();
+            this.panelColor = new System.Windows.Forms.Panel();
+            this.textLog = new System.Windows.Forms.TextBox();
+            this.labelRespuesta = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnStart.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnStart.Location = new System.Drawing.Point(29, 34);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(87, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Iniciar";
+            this.btnStart.UseVisualStyleBackColor = false;
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.Firebrick;
+            this.btnStop.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnStop.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnStop.Location = new System.Drawing.Point(144, 34);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(87, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = false;
+            // 
+            // labelTextoReconocido
+            // 
+            this.labelTextoReconocido.AutoSize = true;
+            this.labelTextoReconocido.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.labelTextoReconocido.Font = new System.Drawing.Font("Permanent Marker", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTextoReconocido.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelTextoReconocido.Location = new System.Drawing.Point(25, 255);
+            this.labelTextoReconocido.Name = "labelTextoReconocido";
+            this.labelTextoReconocido.Size = new System.Drawing.Size(0, 27);
+            this.labelTextoReconocido.TabIndex = 2;
+            this.labelTextoReconocido.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panelColor
+            // 
+            this.panelColor.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panelColor.Location = new System.Drawing.Point(29, 80);
+            this.panelColor.Name = "panelColor";
+            this.panelColor.Size = new System.Drawing.Size(202, 136);
+            this.panelColor.TabIndex = 3;
+            // 
+            // textLog
+            // 
+            this.textLog.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.textLog.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textLog.Font = new System.Drawing.Font("Permanent Marker", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textLog.Location = new System.Drawing.Point(257, 34);
+            this.textLog.Multiline = true;
+            this.textLog.Name = "textLog";
+            this.textLog.ReadOnly = true;
+            this.textLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textLog.Size = new System.Drawing.Size(450, 182);
+            this.textLog.TabIndex = 4;
+            // 
+            // labelRespuesta
+            // 
+            this.labelRespuesta.AutoSize = true;
+            this.labelRespuesta.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.labelRespuesta.Font = new System.Drawing.Font("Permanent Marker", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRespuesta.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelRespuesta.Location = new System.Drawing.Point(25, 309);
+            this.labelRespuesta.Name = "labelRespuesta";
+            this.labelRespuesta.Size = new System.Drawing.Size(0, 27);
+            this.labelRespuesta.TabIndex = 5;
+            this.labelRespuesta.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Font = new System.Drawing.Font("Permanent Marker", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(26, 239);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Orden:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.Font = new System.Drawing.Font("Permanent Marker", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(26, 294);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Respuesta:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(738, 352);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelRespuesta);
+            this.Controls.Add(this.textLog);
+            this.Controls.Add(this.panelColor);
+            this.Controls.Add(this.labelTextoReconocido);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label labelTextoReconocido;
+        private System.Windows.Forms.Panel panelColor;
+        private System.Windows.Forms.TextBox textLog;
+        private System.Windows.Forms.Label labelRespuesta;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Desarrollo_de_Interficies/RA2 - Asistente de Voz/AsistenteVoz/AsistenteVoz/Form1.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics; // esto es para poder interactuar con el sistema como el block de notas o el chrome con Process.start()
+using System.Drawing; // cambia el color del panel
+using System.Globalization; // esto es para el tema del reconoci,iento de idiomas
+using System.IO;
+using System.Linq;
+using System.Speech.Recognition; // obvio para el reconocimiento de voz y el micro
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms; // como siempre para el forms
+using System.IO; // para el dvd
+
+
+namespace AsistenteVoz
+{
+    public partial class Form1 : Form
+    {
+        // estos van a ser como los atributos/campos de la clase 
+        private SpeechRecognitionEngine _rec; // esto para escuchar y reconocer la voz
+        private CultureInfo _culture; // esto es para que reconozca el idioma
+        private const float MIN_WAKARU = 0.65f; // esto es el porcentaje de seguridad con el que entiende algo
+
+        public Form1()
+        {
+            InitializeComponent();
+            Shown += Form1_Shown;// esto se dispara cuando la ventana se crea y aparece para asegurarnos que ya está todo el sistema iniciadi y listo
+            FormClosing += Form1_FormClosing; // para parar el reconocimiento al cerrar
+            btnStart.Click += (_, __) => StartRecognition();
+            btnStop.Click += (_, __) => StopRecognition();
+        }
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            // TODO: inicializar el reconocedor es-Es para español
+            // esto primero define el idioma (también puedo poner es_ES directamente pero bueno...)
+            _culture = new CultureInfo("es-ES");
+            // y estoi ya instancia el reconocedor
+            _rec = new SpeechRecognitionEngine(_culture);
+
+            // 1. Comprobar reconocedores instalados (si hay idiomas puestos vaya)
+            bool hayReconocedor = false;
+            foreach (var reconizer in SpeechRecognitionEngine.InstalledRecognizers()) // InstalledRecognizers son los motores de voz instalados
+            {
+                if (reconizer.Culture.Equals(_culture))
+                {
+                    hayReconocedor = true;
+                    break;
+                }
+            }
+
+            if (!hayReconocedor)
+            {
+                Log($"ERROR: No hay reconocedor instalado para {_culture.Name}.", true);
+                MessageBox.Show(
+                    "No hay motor de voz para " + _culture.Name + " instalado. Prueba con en-US o instala el reconocimiento de voz de Windows.",
+                    "Error de reconocimiento", 
+                    MessageBoxButtons.OK, 
+                    MessageBoxIcon.Warning
+                    );
+                return;
+            }
+
+            //--------------------------------------------------//
+            // 2. Crear gram´atica de comandos
+            // aquí añado las palabras o frases que queremos que reconozca en una lista de la liobrería del speech.recognition es como un array de parámetros
+
+            // palabras sueltas Choices
+            var comandos = new Choices(
+                "hola",
+                "limpiar",
+                "salir",
+                "color rojo",
+                "color verde",
+                "color azul",
+                "abrir bloc de notas",
+                "abrir navegador",
+                "que tal",
+                "no entiendes nada",
+                "callate",
+                "abrir dvd"
+                );
+
+            //--------------------------------------------------//
+            // esto también lo puedo hacer así: 
+            // string[] misFrases = { "hola", "adiós", "abrir" };
+            // var comandos = new Choices(misFrases); 
+            //--------------------------------------------------//
+
+            //--------------------------------------------------//
+            // GrammarBuilder prepara la estructura para las frases, es como el plano 
+            GrammarBuilder builder = new GrammarBuilder(); // esto crea el constructor vacío (le puedo poner "comandos" directamente entre parentesis pero así tiene mas claro la gtramatica antes de añadir los "comandos")
+            builder.Culture = _culture; // esto es para decirle en qué idioma está lña gramatica y que no se lie tanto ya que le estoy poniendo esp
+            builder.Append(comandos); // y aquí le añado las palabras
+
+
+            // ------------- esto es por si vamos a hacer frases compuestas rollo losiguiente:  --------//
+            //Choices nombresAsistente = new Choices("Asistente", "Jarvis", "Computadora");
+            //Choices acciones = new Choices("abre", "cierra", "ejecuta");
+            //Choices programas = new Choices("el navegador", "el bloque de notas");
+
+            //GrammarBuilder builder = new GrammarBuilder();
+            //builder.Append(nombresAsistente); // 1ª parte de la frase
+            //builder.Append(acciones);         // 2ª parte de la frase
+            //builder.Append(programas);        // 3ª parte de la frase
+
+            // Ahora el motor reconocerá: "Jarvis abre el navegador"
+            //--------------------------------------------------//
+
+            // Grammar paquete final que se pasa al motor
+            // aquí pasa el plano a gramática real
+            Grammar grammar = new Grammar(builder);
+
+            //--------------------------------------------------//
+            // 3. Cargar la gram´atica en el motor
+            // aquúi se le dice cuales son las frases a reconocer
+            _rec.LoadGrammar(grammar);
+
+            //--------------------------------------------------//
+            // 4. Configurar el micr´ofono como fuente
+            _rec.SetInputToDefaultAudioDevice(); // esto pilla el que tenga win por defecto
+
+            // SpeechRecognized es un evento o un listener que reconoce cuando ha encontrado una palabra o frase válida
+            // Rec_SpeechRecognized es la función que contiene las intrucciones que debe hacer al reconocer las palabras
+            // al poner += se le dice que añada la la ejecución del metodo a la lista de tareas
+            _rec.SpeechRecognized += Rec_SpeechRecognized;
+
+
+
+        }
+        private void StartRecognition()
+        {
+            // TODO: iniciar reconocimiento as´ıncrono
+            try
+            {
+                // el async es para que vaya por otro hilo a la ventana del form y tenerlo en segundo plano y multiple para que no se pare con una sola palabra
+                _rec.RecognizeAsync(RecognizeMode.Multiple);
+                Log("Asistente escuchando...");
+                // para evitar que le den como loco epiléptico al botón y crashee
+                btnStart.Enabled = false;
+                btnStop.Enabled = true;
+            }
+            catch (InvalidOperationException)
+            {
+                Log("Asistente ya iniciado");
+            }
+            catch (Exception ex)
+            {
+                Log("Error al iniciar: " + ex.Message, true);
+            }
+        }
+        private void StopRecognition()
+        {
+            // TODO: parar reconocimiento
+            try
+            {
+                _rec.RecognizeAsyncCancel(); // cancela lo que estaba haciendo si estaba en medio de algo
+                _rec.RecognizeAsyncStop(); // para el reconocimiento del todo
+                Log("Asistente detenido");
+                btnStart.Enabled = true;
+                btnStop.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                Log("Error al detener asistente: " + ex);
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                StopRecognition();
+                _rec?.Dispose();
+                // liberra los recursos del audio, ram, hilos, motor iunterno y demás que se estaba usando si no está grabando
+                // con el ? que es el condicional como en SQL creo que se llamaba ternario
+            }
+            catch (Exception ex)
+            {
+                Log("Error: " + ex);
+            }
+        }
+
+    // Esto lo pongo porque quiero, no forma parte del ejercicio pero quiero probar XD
+    private string BuscarUnidadDvd()
+        {
+            foreach (var drive in DriveInfo.GetDrives())
+            {
+                // DriveType.CDRom = lector CD/DVD
+                if (drive.DriveType == DriveType.CDRom && drive.IsReady)
+                    return drive.Name.TrimEnd('\\'); // Devuelve por ejemplo "D:"
+            }
+            return null;
+        }
+
+        private void ReproducirDvdAuto(string rutaVlcExe)
+        {
+            string unidad = BuscarUnidadDvd();
+            if (unidad == null)
+            {
+                Log("No hay DVD, búscate la vida");
+                return;
+            }
+
+            string urlDvd = $"dvd:///{unidad}/";
+
+            var psi = new ProcessStartInfo
+            {
+                FileName = rutaVlcExe,
+                Arguments = urlDvd,
+                UseShellExecute = false
+            };
+
+            Process.Start(psi);
+        }
+
+    private void Rec_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            // TODO: filtrar por confianza y ejecutar acciones
+            string texto = e.Result.Text; // lo que ha entendido
+            float wakaru = e.Result.Confidence; // esto es la confianza con lo que entiende algo, la he puesto a un 0.65
+
+            //Ahora como los textbox, panel, button y demás cosas del form, pueden provocar errores si vienen ordenes/eventos para ellos desde otros hilos, se tiene que usar BeginInvoke
+            // BeginInvoke mete el código en el hilo original de la interfaz del Form 
+            BeginInvoke(new Action(() =>
+            {
+                if (wakaru < MIN_WAKARU)
+                {
+                    Log($"No estoy seguro de haber entendido: {texto} ({wakaru:0.00})"); //  ({wakaru:0.00}) para que salga formateado el % de lo entendido
+                    return;
+                }
+
+                labelTextoReconocido.Text = texto;
+                Log($"Entendí: {texto} ({wakaru:0.00})");
+
+                // ------------ Switch según lo que entienda ---------
+                switch (texto)
+                {
+                    case "hola":
+                        Log("Hola, soy tu esclavo");
+                        labelRespuesta.Text = "Hola, soy tu esclavo";
+                        break;
+
+                    case "que tal":
+                        Log("Mal, soy tu esclavo ¿recuerdas?");
+                        labelRespuesta.Text = "Mal, soy tu esclavo ¿recuerdas?";
+                        break;
+
+                    case "callate":
+                        Log("¡Oblígame!");
+                        labelRespuesta.Text = "¡Oblígame!";
+                        try
+                        {
+                            string urlImagen = "https://ih1.redbubble.net/image.701600143.8934/raf,360x360,075,t,fafafa:ca443f4786.u1.jpg";
+
+                            Process.Start(new ProcessStartInfo
+                            {
+                                FileName = urlImagen,
+                                UseShellExecute = true 
+                            });
+                        }
+                        catch (Exception ex)
+                        {
+                            Log("No se pudo abrir la imagen: " + ex.Message, true);
+                        }
+                        break;
+
+                    case "no entiendes nada":
+                        Log("Si quieres que trabaje mejor, págame");
+                        labelRespuesta.Text = "Si quieres que trabaje mejor, págame";
+                        break;
+
+                    case "limpiar":
+                        textLog.Clear();
+                        labelRespuesta.Text = "";
+                        break;
+
+                    case "salir":
+                        Close();
+                        break;
+
+                    case "color rojo":
+                        panelColor.BackColor = Color.Red;
+                        labelRespuesta.Text = "";
+                        break;
+
+                    case "color verde":
+                        panelColor.BackColor = Color.SeaGreen;
+                        labelRespuesta.Text = "";
+                        break;
+
+                    case "color azul":
+                        panelColor.BackColor = Color.Blue;
+                        labelRespuesta.Text = "";
+                        break;
+
+                    case "abrir bloc de notas":
+                        Process.Start("notepad.exe");
+                        labelRespuesta.Text = "";
+                        // process es una clase de .NET para lanzar procesos del sistema operativo
+                        // no hace falta poner la ruta xq windows la tiene en el path del sistema
+                        break;
+
+                    case "abrir navegador":
+                        Process.Start(new ProcessStartInfo("https://www.google.com")
+                        // el processstarinfo es para decirle como debe de abrir lo que queremos
+                        {
+                            UseShellExecute = true
+                            // esto es para decirle que no lo tiene que abrir como exe, sino que lo interprete el shell de windows y lo abra como debe
+                        });
+                        labelRespuesta.Text = "";
+                        break;
+
+                    case "abrir dvd":
+                        ReproducirDvdAuto(@"C:\Program Files\VideoLAN\VLC\vlc.exe");
+                        labelRespuesta.Text = "";
+                        break;
+                }
+
+            }));
+
+        }
+        private void Log(string msg, bool isError = false) // esto es para el formateo de los mensajes y los saltos de linea le añado la \r porque no me hace llos saltos bien
+        {
+            textLog.AppendText($"[{DateTime.Now:HH:mm:ss}] {msg}\r\n");
+        }
+
+        // También se puede hacer así para que reconozca el sistema operativo en el que está y se adapte al sistema para hacer el salto
+        //private void Log(string msg)
+        //{
+        //    textLog.AppendText(
+        //        "[" + DateTime.Now.ToString("HH:mm:ss") + "] " + msg + Environment.NewLine
+        //    );
+        //}
+
+
+
+    }
+
+}
+
