0ab9c287fd9ab35e6a15fc5148dc0f2447191dcd
Kuroimichan4 <miriamhernav@campus.monlau.com>
2026-03-27 14:51:12 +0100

    game actu

--- Programación Multimedia y Dispositivos Móviles/Game/main/java/com/example/game/VistaPropia.java
+++ Programación Multimedia y Dispositivos Móviles/Game/main/java/com/example/game/VistaPropia.java
@@ -9,19 +9,26 @@ import android.view.MotionEvent; // esto es para que el usuario pueda tocar la p
 
 public class VistaPropia extends View {
 
-    int posX = 550; //alto
-    int posY = 300; // ancho
+    int posX = 550; //alto pero visto de lado
+    int posY = 2000; // ancho
     int radio = 100;
 
     // ------------ esto lo dejo por si lo aprovecho para el movimiento de las naves enemigas
     int velocidadX = 0; //velocidad lateral
-    int velocidadY = 0; // velocidad vertical
+    int velocidadY = 12; // velocidad vertical
 
     int puntuacion = 0;
-    // - Enemigo - //
-    int enemigoX = 550;
-    int enemigoY = 1000;
+
+    // - Enemigos - //
     int enemigoRadio = 100;
+    int cantidadEnemigos = 3;
+    int[] enemigoX = new int[cantidadEnemigos];
+    int[] enemigoY = new int[cantidadEnemigos];
+    boolean[] enemigosActivos = new boolean[cantidadEnemigos];
+    boolean juegoIniciado = false;
+    boolean gameOver = false;
+
+//    int enemigoY = -enemigoRadio;
 
     //- pincel-//
     Paint pincel = new Paint();
@@ -46,48 +53,86 @@ public class VistaPropia extends View {
         int ancho = canvas.getWidth(); //ancho de la pantalla para no pintar una línea infinita y se adapta a todas las pantallas
         int alto = canvas.getHeight(); // alto total de la pantalla
 
+        // - Activa enemigos si no hay ninguno activo - //
+        if (!juegoIniciado) {
+
+            for (int i = 0; i < cantidadEnemigos; i++) {
+                enemigoX[i] = (int) (Math.random() * (ancho - 2 * enemigoRadio)) + enemigoRadio;
+                enemigoY[i] = (-200 * i) - (int)(Math.random() * 500); // salen en -200 * el numero del array y se resta el aleatorio * 200 para que salgan escalonados
+                enemigosActivos[i] = true;
+
+            }
+            juegoIniciado = true;
+        }
+
         // - circulo - //
         pincel.setARGB(255, 0, 0, 0); // Negro
         canvas.drawCircle(posX, posY , radio, pincel); // dibuja circulos drawCircle(centroX, centroY, radio, paint).
 
         // - enemigo - //
         pincel.setARGB(255, 255, 0, 0); // Rojo
-        canvas.drawCircle(enemigoX, enemigoY, enemigoRadio, pincel);
+
+        for (int i = 0; i < cantidadEnemigos; i++) {
+            if (enemigosActivos[i]) {
+                canvas.drawCircle(enemigoX[i], enemigoY[i], enemigoRadio, pincel);
+            }
+        }
+
+        // - Posición y movimiento automático Enemigo - //
+        for (int i = 0; i < cantidadEnemigos; i++) {
+            if (enemigosActivos[i]) {
+                enemigoY[i] = enemigoY[i] + velocidadY; // esto mueve el objeto en pantalla y se repite
+            }
+
+            // Para hacer un enemigo perseguidor -----------**********
+//            if (enemigoX < posX) {
+//                enemigoX += 3;
+//            } else if (enemigoX > posX) {
+//                enemigoX -= 3;
+//            }
+//
+//            enemigoY += velocidadY;
+
+        }
 
         // - puntuación - // ** Hay que cambiarlo xq el juego va a ser en horizontal
         pincel.setARGB(255, 255, 255, 255); // Blanco
         pincel.setTextSize(80);
         canvas.drawText("Puntos: " + puntuacion , 100, 100, pincel);
 
-        // - distancia entre circulos - //
-        int distX = posX - enemigoX;
-        int distY = posY - enemigoY;
-        double distancia = Math.sqrt(distX * distX + distY * distY); // sqrt es para sacar la raiz cuadrada y despejar la distancia
+        // - Distancia entre enemigo - //
+        for (int i = 0; i < cantidadEnemigos; i++) {
+            if (enemigosActivos[i]) {
+                int distX = posX - enemigoX[i];
+                int distY = posY - enemigoY[i];
 
-        if (distancia < radio + enemigoRadio) { // si la distancia entre los 2 es menor al radio que ocupan, se chocan
-            puntuacion = puntuacion + 1;
+                // - distancia entre circulos - //
+                double distancia = Math.sqrt(distX * distX + distY * distY); // sqrt es para sacar la raiz cuadrada y despejar la distancia
 
-            enemigoX = 550;
-            enemigoY = 1100;
+                if (distancia < radio + enemigoRadio) { // si la distancia entre los 2 es menor al radio que ocupan, se chocan
+                    gameOver = true;
+                }
+            }
         }
 
-
-        //- movimiento automático-// ** igual se queda para el movimiento de las naves enemigas
-        posX = posX + velocidadX; // esto mueve el objeto en pantalla y se repite
-        posY = posY + velocidadY;
-
-        // - detector de bordes - //
-        // ancho
-        if (posX + radio >= ancho || posX - radio <= 0) {
-            velocidadX = -velocidadX;
+        // - Respawn -//
+        for (int i = 0; i < cantidadEnemigos; i++) {
+            if (!enemigosActivos[i]) {
+                enemigoX[i] = (int) (Math.random() * (ancho  - 2 * enemigoRadio)) + enemigoRadio;
+                enemigoY[i] = (-200 * i) - (int)(Math.random() * 500);
+                enemigosActivos[i] = true;
+            }
         }
-        // alto
-        if (posY + radio >= alto || posY - radio <= 0) {
-            velocidadY = -velocidadY;
+
+        if (gameOver) {
+            canvas.drawRGB(0, 0, 0); // para quitar el juego y pintar un fondo negro
+            pincel.setARGB(255, 255, 0, 0); // Rojo
+            pincel.setTextSize(120);
+            canvas.drawText("GAME OVER", ancho / 2 - 310, alto / 2, pincel);
+            return; // para no seguir actualizando la pantalla, solo mostrará el game over
         }
 
         invalidate(); // esto hace que repinte la pantalla y entre en bucle, repintado y sumando continuamente y no para de moverse
-
     }
 
     @Override
@@ -100,7 +145,21 @@ public class VistaPropia extends View {
 
                 // obtenemos la posición del toque
                 posX = (int) event.getX();
-                posY = (int) event.getY();
+                //posY = (int) event.getY();
+
+                // - Para que no se salga de la pantalla - //
+                if (posX - radio < 0) {
+                    posX = radio; // para que la posición no sea negativa y salga de los límites se pone como mínimo el radio de la nave
+                }
+                if (posX + radio > getWidth()) {
+                    posX = getWidth() - radio; // para que no sea myor a la pantalla es igual al tamaño menos el radio
+                }
+//                if (posY - radio < 0) {
+//                    posY = radio;
+//                }
+//                if (posY + radio > getHeight()) {
+//                    posY = getHeight() - radio;
+//                }
 
                 //repintar al tocar
                 invalidate();
